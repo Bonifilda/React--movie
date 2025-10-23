913f32c97e4127b8cc1eb9013c5929b0f12d4ae7
usanase12 <usanasesandra22@gmail.com>
2025-10-23 08:48:25 +0200

    file arrangement

--- my-app/src/pages/Favorites.jsx
+++ my-app/src/pages/Favorites.jsx
@@ -7,7 +7,7 @@ const Favorites = () => {
 
   return (
     <div className="min-h-screen bg-gray-950 text-white px-6 py-10">
-      <h1 className="text-3xl md:text-4xl font-bold mb-8 text-center text-sky-400">Your Favorite Movies</h1>
+      <h1 className="text-2xl md:text-4xl font-bold mb-8 text-center text-sky-400">Your Favorite Movies</h1>
       
       {favorites.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
