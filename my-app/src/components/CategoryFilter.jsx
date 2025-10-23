a4d4dc5b7b2fc91789a02575bf2c196a494db295
usanase12 <usanasesandra22@gmail.com>
2025-10-23 04:59:22 +0200

    Styled MovieCard component with Tailwind CSS and made it responsive

--- my-app/src/components/CategoryFilter.jsx
+++ my-app/src/components/CategoryFilter.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const CategoryFilter = ({ categories, selectedCategory, setSelectedCategory }) => {
   return (
-    <div className=" flex flex-wrap  justify-center ... text-indigo-500 text-sm font-medium  ">
-      <h3>Filter by Genre</h3>
-      <div className=" ">
+    <div className=" flex flex-col sm:flex-row flex-wrap justify-center items-center gap-4 bg-pink-900 text-white px-6 py-4 rounded-2xl shadow-md ">
+      <h3 className="text-sky-400 font-semibold text-lg">Filter by Genre</h3>
+      <div className=" flex flex-wrap justify-center gap-3 space-x-2">
         <button 
           className={selectedCategory === '' ? 'active' : ''} 
           onClick={() => setSelectedCategory('')}
@@ -14,7 +14,7 @@ const CategoryFilter = ({ categories, selectedCategory, setSelectedCategory }) =
         {categories.map(category => (
           <button 
             key={category} 
-            className={selectedCategory === category ? 'active' : ''}
+            className={  selectedCategory === category ? 'active' : ''}
             onClick={() => setSelectedCategory(category)}
           >
             {category}
--- my-app/src/components/MovieCard.jsx
+++ my-app/src/components/MovieCard.jsx
@@ -6,7 +6,7 @@ const MovieCard = ({ movie, addToFavorites, isFavorite }) => {
   const imageUrl = movie.image?.medium || 'https://via.placeholder.com/210x295?text=No+Image';
   
   return (
-    <div className="bg-gray-900 rounded-2xl overflow-hidden shadow-lg hover:shadow-2xl transform hover:scale-105 transition duration-300 flex flex-col ">
+    <div className="bg-pink-900 rounded-2xl overflow-hidden shadow-lg hover:shadow-2xl transform hover:scale-105 transition duration-300 flex flex-col ">
       <img src={imageUrl} alt={movie.name} className="w-full h-72 object-cover" />
       <div className="p-4 flex flex-col  justify-between">
         <h3 className="text-lg font-semibold text-white mb-1 line-clamp-1">{movie.name}</h3>
